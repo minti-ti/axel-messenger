b6168135c7d57135e85e0af5564ef66f1ec8f764
Minti <y356500@yandex.ru>
2026-05-30 17:23:05 +0300

    fix: remove extra parenthesis in server.js log.info

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const http = require('http');
 const path = require('path');
 const rateLimit = require('express-rate-limit');
